f293b1ccd0f586ad248ab363880707a8ade1570d
Tuomas Marenk <tuomas.marenk@gmail.com>
2022-09-17 16:07:36 +0300

    Lisatty liikkuminen peliin

---
+++ src/peli/liiku.py
@@ -0,0 +1,148 @@
+"""2048-pelin liikkumisesta vastaava koodi
+"""
+
+def liiku_vasen(taulukko: list):
+    k = 0
+    for i in taulukko:
+        edellinen = None
+        e_paikka = None
+        tyhja = None
+        for j in range(4):
+            if i[j] == 0:
+                if tyhja == None:
+                    tyhja = j
+            elif edellinen != i[j]:
+                edellinen = i[j]
+                e_paikka = j
+                if tyhja != None:
+                    taulukko[k][tyhja] = i[j]
+                    taulukko[k][j] = 0
+                    e_paikka = tyhja
+                    tyhja += 1
+            else:
+                if tyhja != None and tyhja < e_paikka:
+                    taulukko[k][tyhja] = i[j] * 2
+                    edellinen = None
+                    taulukko[k][e_paikka] = 0
+                    taulukko[k][j] = 0
+                    tyhja += 1
+                elif tyhja != None and tyhja > e_paikka:
+                    taulukko[k][e_paikka] = i[j] * 2
+                    edellinen = None
+                    taulukko[k][j] = 0
+                else:
+                    taulukko[k][e_paikka] = i[j] * 2
+                    edellinen = None
+                    taulukko[k][j] = 0
+                    tyhja = j
+        k += 1
+    return taulukko
+
+def liiku_oikea(taulukko: list):
+    k = 0
+    for i in taulukko:
+        edellinen = None
+        e_paikka = None
+        tyhja = None
+        for j in range(3,-1,-1):
+            if i[j] == 0:
+                if tyhja == None:
+                    tyhja = j
+            elif edellinen != i[j]:
+                edellinen = i[j]
+                e_paikka = j
+                if tyhja != None:
+                    taulukko[k][tyhja] = i[j]
+                    taulukko[k][j] = 0
+                    e_paikka = tyhja
+                    tyhja -= 1
+            else:
+                if tyhja != None and tyhja > e_paikka:
+                    taulukko[k][tyhja] = i[j] * 2
+                    edellinen = None
+                    taulukko[k][e_paikka] = 0
+                    taulukko[k][j] = 0
+                    tyhja -= 1
+                elif tyhja != None and tyhja < e_paikka:
+                    taulukko[k][e_paikka] = i[j] * 2
+                    edellinen = None
+                    taulukko[k][j] = 0
+                else:
+                    taulukko[k][e_paikka] = i[j] * 2
+                    edellinen = None
+                    taulukko[k][j] = 0
+                    tyhja = j
+        k += 1
+    return taulukko
+
+def liiku_ylos(taulukko: list):
+    for j in range(4):
+        edellinen = None
+        e_paikka = None
+        tyhja = None
+        for k in range(4):
+            i = taulukko[k]
+            if i[j] == 0:
+                if tyhja == None:
+                    tyhja = k
+            elif edellinen != i[j]:
+                edellinen = i[j]
+                e_paikka = k
+                if tyhja != None:
+                    taulukko[tyhja][j] = i[j]
+                    taulukko[k][j] = 0
+                    e_paikka = tyhja
+                    tyhja += 1
+            else:
+                if tyhja != None and tyhja < e_paikka:
+                    taulukko[tyhja][j] = i[j] * 2
+                    edellinen = None
+                    taulukko[e_paikka][j] = 0
+                    taulukko[k][j] = 0
+                    tyhja += 1
+                elif tyhja != None and tyhja > e_paikka:
+                    taulukko[e_paikka][j] = i[j] * 2
+                    edellinen = None
+                    taulukko[k][j] = 0
+                else:
+                    taulukko[e_paikka][j] = i[j] * 2
+                    edellinen = None
+                    taulukko[k][j] = 0
+                    tyhja = j
+    return taulukko
+
+def liiku_alas(taulukko: list):
+    for j in range(4):
+        edellinen = None
+        e_paikka = None
+        tyhja = None
+        for k in range(3, -1, -1):
+            i = taulukko[k]
+            if i[j] == 0:
+                if tyhja == None:
+                    tyhja = k
+            elif edellinen != i[j]:
+                edellinen = i[j]
+                e_paikka = k
+                if tyhja != None:
+                    taulukko[tyhja][j] = i[j]
+                    taulukko[k][j] = 0
+                    e_paikka = tyhja
+                    tyhja -= 1
+            else:
+                if tyhja != None and tyhja > e_paikka:
+                    taulukko[tyhja][j] = i[j] * 2
+                    edellinen = None
+                    taulukko[e_paikka][j] = 0
+                    taulukko[k][j] = 0
+                    tyhja -= 1
+                elif tyhja != None and tyhja < e_paikka:
+                    taulukko[e_paikka][j] = i[j] * 2
+                    edellinen = None
+                    taulukko[k][j] = 0
+                else:
+                    taulukko[e_paikka][j] = i[j] * 2
+                    edellinen = None
+                    taulukko[k][j] = 0
+                    tyhja = j
+    return taulukko
--- src/peli/peli.py
+++ src/peli/peli.py
@@ -1,7 +1,38 @@
 from random import randint
+from tkinter import W
+from liiku import liiku_vasen, liiku_oikea, liiku_ylos, liiku_alas
 
-"""2048-pelin muodostava koodi
+"""2048-pelin rungosta vastaava koodi
 """
+
+def uusi_peli():
+    """Funktio, joka luo uuden pelin
+    
+    Returns:
+        Uuden peli-ruudukon
+    """
+    taulukko = [[0]*4 for i in range(4)]
+    edellinen = None
+    for i in range(2):
+        while True:
+            sijoitus_y = randint(0,3)
+            sijoitus_x = randint(0,3)
+            valinta = randint(1, 10)
+            if valinta == 9:
+                uusi = 4
+            else:
+                uusi = 2
+            if edellinen:
+                if (sijoitus_y,sijoitus_x)==edellinen:
+                    continue
+                else:
+                    taulukko[sijoitus_y][sijoitus_x] = uusi
+                    break
+            else:
+                taulukko[sijoitus_y][sijoitus_x] = uusi
+                edellinen = (sijoitus_y,sijoitus_x) 
+                break
+    return taulukko
 
 def katso_ylos_alas(suunta: str, taulukko: list):
     """Fuktio, joka katsoo, voiko ruudukkoa liikuttaa ylös tai alas
@@ -13,19 +44,18 @@ def katso_ylos_alas(suunta: str, taulukko: list):
     Returns:
         Totuusarvon siitä, voiko ruudukkoa liikuttaa kyseiseen suuntaan
     """
+
     totuudet = {1: False, 2:False, 3:False, 4:False}
     if suunta == "ylos":
         a = 0
         b = 4
         c = 1
-        d = 1
     elif suunta == "alas":
         a = 3
         b = -1
         c = -1
-        d = -1
+    k = 1
     for j in range(4):
-        k = 1
         edellinen = None
         for i in range(a,b,c):
             i = taulukko[i]
@@ -72,7 +102,6 @@ def katso_vasen_oikea(suunta: str, taulukko: list):
         k = 1
         edellinen = None
         for j in range(b,c,d):
-            print(i[j])
             if i[j] == 0:
                 totuudet[k] = True
             elif edellinen == None:
@@ -86,35 +115,6 @@ def katso_vasen_oikea(suunta: str, taulukko: list):
                 edellinen = i[j]
         k += 1
     return False
-
-def uusi_peli():
-    """Funktio, joka luo uuden pelin
-    
-    Returns:
-        Uuden peli-ruudukon
-    """
-    taulukko = [[0]*4 for i in range(4)]
-    edellinen = None
-    for i in range(2):
-        while True:
-            sijoitus_y = randint(0,3)
-            sijoitus_x = randint(0,3)
-            valinta = randint(1, 10)
-            if valinta == 9:
-                uusi = 4
-            else:
-                uusi = 2
-            if edellinen:
-                if (sijoitus_y,sijoitus_x)==edellinen:
-                    continue
-                else:
-                    taulukko[sijoitus_y][sijoitus_x] = uusi
-                    break
-            else:
-                taulukko[sijoitus_y][sijoitus_x] = uusi
-                edellinen = (sijoitus_y,sijoitus_x) 
-                break
-    return taulukko
 
 def tulosta_taulukko(taulukko):
     """Funktio, joka tulostaa peli-ruudukon
@@ -162,16 +162,19 @@ def main():
     """Peliä pyörittävä funktio
     """
 
-    taulukko = uusi_peli()
-    tulosta_taulukko(taulukko)
-    taulukko = uusi_palikka(taulukko)
-    tulosta_taulukko(taulukko)
-    taulukko = [[4,2,2,8],[4,0,0,0],[0,0,0,0],[0,0,0,0]]
-    print(katso_vasen_oikea("vasen", taulukko))
-    print(katso_vasen_oikea("oikea", taulukko))
-    print(katso_ylos_alas("ylos", taulukko))
-    print(katso_ylos_alas("alas", taulukko))
+    pass
 
 if __name__ == "__main__":
-    main()
-    
+    #taulukko = uusi_peli()
+    #tulosta_taulukko(taulukko)
+    #taulukko = uusi_palikka(taulukko)
+    #tulosta_taulukko(taulukko)
+    taulukko = [[4,2,2,8],[8,0,4,0],[4,4,8,8],[2,0,16,16]]
+    #print(katso_vasen_oikea("vasen", taulukko))
+    #print(katso_vasen_oikea("oikea", taulukko))
+    #print(katso_ylos_alas("ylos", taulukko))
+    #print(katso_ylos_alas("alas", taulukko))
+    tulosta_taulukko(taulukko)
+    taulukko = liiku_alas(taulukko)
+    tulosta_taulukko(taulukko)
+
